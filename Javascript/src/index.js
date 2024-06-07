f680ec624afca1d1b6d66577c3034592574c469b
Ravi Kallan <r.kallan@icloud.com>
2024-06-08 00:10:37 +0200

    items data as json object

--- Javascript/src/index.js
+++ Javascript/src/index.js
@@ -1,1 +1,5 @@
+import dataItems from "./resources/data/items.js";
+
+console.log(dataItems.length);
+
 console.log("AXXES CODE KATA - GILDED TROS");
---
+++ Javascript/src/resources/data/items.js
@@ -0,0 +1,59 @@
+const items = [
+    {
+        name: "Ring of Cleansening Code",
+        sellIn: 10,
+        quality: 20,
+    },
+    {
+        name: "Good Wine",
+        sellIn: 2,
+        quality: 0,
+    },
+    {
+        name: "Elixir of the SOLID",
+        sellIn: 5,
+        quality: 7,
+    },
+    {
+        name: "B-DAWG Keychain",
+        sellIn: 0,
+        quality: 80,
+    },
+    {
+        name: "B-DAWG Keychain",
+        sellIn: -1,
+        quality: 80,
+    },
+    {
+        name: "Backstage passes for Re:Factor",
+        sellIn: 15,
+        quality: 20,
+    },
+    {
+        name: "Backstage passes for Re:Factor",
+        sellIn: 10,
+        quality: 49,
+    },
+    {
+        name: "Backstage passes for HAXX",
+        sellIn: 5,
+        quality: 49,
+    },
+    {
+        name: "Duplicate Code",
+        sellIn: 3,
+        quality: 6,
+    },
+    {
+        name: "Long Methods",
+        sellIn: 3,
+        quality: 6,
+    },
+    {
+        name: "Ugly Variable Names",
+        sellIn: 3,
+        quality: 6,
+    },
+];
+
+export default items;
