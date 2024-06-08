cfa441e0567da67d8a2f84ab92d68cb2926acf53
Ravi Kallan <r.kallan@icloud.com>
2024-06-08 07:43:54 +0200

    items values check

--- Javascript/src/items/index.js
+++ Javascript/src/items/index.js
@@ -1,12 +1,17 @@
+import { getType, getCategory, getQuality } from "../helpers/index.js";
 import { Item } from "./item.js";
-import { getType } from "../helpers/index.js";
-
 import dataItems from "./data/items.js";
 
 const getItems = () => {
     if (getType(dataItems) !== "array") return [];
 
-    const items = dataItems.map(({ name, sellIn, quality }) => new Item(name, sellIn, quality));
+    const items = dataItems.map(({ name, ...item }) => {
+        const category = getCategory(name);
+        const sellIn = category === "legendary" ? null : Number(item.sellIn) || 0;
+        const quality = category === "legendary" ? 80 : getQuality(item.quality);
+
+        return new Item(name, sellIn, quality);
+    });
 
     return items;
 };
